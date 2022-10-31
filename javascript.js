f1fdf75d2d813c08225a1a31cfd730ad20e42953
Maria "Saydee" Guevarra <114355214+SaydeeG5@users.noreply.github.com>
2022-10-30 18:44:57 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,10 +3,10 @@ let country = "USA";
 let myName = "Saydee";
 let lastName = "Guevara";
 let email = "saydeeguevarra@gmail.com";
-let husbandname = "James";
-let child1name = "Dominic";
-let child2name = "Roman";
-let child3name = "Luke";
+let husbandName = "James";
+let child1Name = "Dominic";
+let child2Name = "Roman";
+let child3Name = "Luke";
 let city = "Las Vegas";
 let hobbies = "workout";
 let branch = "Army";
@@ -19,7 +19,7 @@ let costume3 = "Iron Man";
 let place =  "Universal Studios";
 let placebecause = "Mario World";
 let video = "YouTube";
-let youtubename = "Guevarra5";
+let youtubeName = "Guevarra5";
 let professor = "Professor Sam"
 let day = "Tuesday";
 
@@ -37,12 +37,12 @@ document.write("<p>" + myName + " lives in " + country + "</p>");
 document.write(`
 <p style  = "color : blue"> Name: ${myName}</p>
 <p style  = "color : blue"> Last Name: ${lastName}</p>
-<p style  = "font-size : 24"> Husband's Name: ${husbandname}</p>
-<p style  = "font-size : 24"> Child 1: ${child1name}</p>
-<p style  = "font-size : 24"> Child 2: ${child2name}</p>
-<p style  = "font-size : 24"> Child 3: ${child3name}</p>
+<p style  = "font-size : 24"> Husband's Name: ${husbandName}</p>
+<p style  = "font-size : 24"> Child 1: ${child1Name}</p>
+<p style  = "font-size : 24"> Child 2: ${child2Name}</p>
+<p style  = "font-size : 24"> Child 3: ${child3Name}</p>
 <p style  = "color : blue"> We have a  ${video}.</p>
-<p style  = "color : blue"> The name of our ${video} is ${youtubename}.</p>
+<p style  = "color : blue"> The name of our ${video} is ${youtubeName}.</p>
 <p style  = "font-size : 20"> We live in ${city}.</p>
 <p style  = "color : pink"> I am  ${age} years old.</p>
 <p style  = "text-decoration : underline"> We are a ${branch} family.</p>
@@ -50,9 +50,9 @@ document.write(`
 <p style  = "font-weight : 600"> Our bulldog's name is  ${bulldog}.</p>
 <p style  = "color : orange">We can't wait to go trick or treatin' this ${holiday}!</p>
 <p style  = "color : red"> Thank you EVERYONE and  ${professor} for moving our class to ${day}.</p>
-<p style  = "font-size : 24"> ${child1name} is going to be ${costume1}.</p>
-<p style  = "font-size : 24"> ${child2name} is going to be ${costume2}.</p>
-<p style  = "font-size : 24"> ${child3name} is going to be ${costume3}.</p>
+<p style  = "font-size : 24"> ${child1Name} is going to be ${costume1}.</p>
+<p style  = "font-size : 24"> ${child2Name} is going to be ${costume2}.</p>
+<p style  = "font-size : 24"> ${child3Name} is going to be ${costume3}.</p>
 <p style  = "color : teal"> Next year we plan to go to ${place}.</p>
 <p style  = "font-size : 24"> Because ${place} is going to open ${placebecause}!</p>
 `);
